8eceb0bbe4e87966506e627796c0cbdb2049599e
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2023-04-09 04:38:48 -0300

    Página de Cadastro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './pages/Home'
 import Clientes from './pages/Clientes'
 import Config from './pages/Config'
 import Login from './pages/Login'
+import Cadastrar from './pages/CadastrarUsuario'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/clientes" element={<Clientes />} />
           <Route path="/config" element={<Config />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/nova-conta" element={<Cadastrar />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/CadastrarUsuario/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {
+  Container,
+  TopDiv,
+  LoginForm,
+  BottomDiv,
+  Label,
+  Input,
+  NavLink,
+  NewAccount,
+} from './styles'
+import { useNavigate } from 'react-router-dom'
+
+const Cadastrar = () => {
+  const navigate = useNavigate()
+
+  function handleClick() {
+    navigate('/login')
+  }
+
+  return (
+    <Container>
+      <TopDiv>CADASTRAR-SE</TopDiv>
+      <BottomDiv>
+        <LoginForm>
+          <Label>Email</Label>
+          <Input type="text" placeholder="email@email.com" />
+
+          <Label style={{ marginTop: '20px' }}>Senha</Label>
+          <Input type="password" placeholder="************" />
+
+          <Label style={{ marginTop: '20px' }}>Confirmar Senha</Label>
+          <Input type="password" placeholder="************" />
+
+          <NavLink to="/">Cadastrar</NavLink>
+
+          <NewAccount onClick={handleClick}>
+            <p>Já tem uma conta? Entre</p>
+          </NewAccount>
+        </LoginForm>
+      </BottomDiv>
+    </Container>
+  )
+}
+
+export default Cadastrar
