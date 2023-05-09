7809d444d493734336688323f4243d16e06d477c
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2023-05-09 15:33:47 -0300

    Página Home: lista de chamados

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,4 +1,6 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Paper from '@mui/material/Paper'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
@@ -18,30 +20,42 @@ import { HeaderTable } from './styles'
 import { useNavigate } from 'react-router-dom'
 
 export default function StickyHeadTable() {
-  const [page, setPage] = React.useState(0)
+  const [page, setPage] = useState(0)
   const [rowsPerPage, setRowsPerPage] = React.useState(10)
+  const [chamados, setChamados] = React.useState([])
 
   const navigate = useNavigate()
 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        'http://localhost:8000/chamados?_sort=dataCriacao&_order=desc',
+      )
+      const data = response.data
+      setChamados(data.reverse())
+    }
+    fetchData()
+  }, [])
+
   const columns = [
-    { id: 'name', label: 'Cliente', minWidth: 170, align: 'center' },
-    { id: 'code', label: 'Assunto', minWidth: 100, align: 'center' },
+    { id: 'nome', label: 'Cliente', minWidth: 170, align: 'center' },
+    { id: 'assunto', label: 'Assunto', minWidth: 100, align: 'center' },
     {
-      id: 'population',
+      id: 'status',
       label: 'Status',
       minWidth: 170,
       align: 'center',
       format: (value) => value.toLocaleString('en-US'),
     },
     {
-      id: 'size',
+      id: 'data',
       label: 'Data',
       minWidth: 170,
       align: 'center',
       format: (value) => value.toLocaleString('en-US'),
     },
     {
-      id: 'density',
+      id: 'acoes',
       label: 'Ações',
       minWidth: 170,
       align: 'center',
@@ -63,113 +77,13 @@ export default function StickyHeadTable() {
   }
   function handleView(row) {}
 
-  const rows = [
-    {
-      name: 'Agência MKT',
-      code: 'Suporte',
-      population: 'Progresso',
-      size: '23/03/2023',
-      density: '',
-    },
-    {
-      name: 'China',
-      code: 'CN',
-      population: 1403500365,
-      size: 9596961,
-      density: '',
-    },
-    {
-      name: 'Italy',
-      code: 'IT',
-      population: 60483973,
-      size: 301340,
-      density: '',
-    },
-    {
-      name: 'United States',
-      code: 'US',
-      population: 327167434,
-      size: 9833520,
-      density: '',
-    },
-    {
-      name: 'Canada',
-      code: 'CA',
-      population: 37602103,
-      size: 9984670,
-      density: '',
-    },
-    {
-      name: 'Australia',
-      code: 'AU',
-      population: 25475400,
-      size: 7692024,
-      density: '',
-    },
-    {
-      name: 'Germany',
-      code: 'DE',
-      population: 83019200,
-      size: 357578,
-      density: '',
-    },
-    {
-      name: 'Ireland',
-      code: 'IE',
-      population: 4857000,
-      size: 70273,
-      density: '',
-    },
-    {
-      name: 'Mexico',
-      code: 'MX',
-      population: 126577691,
-      size: 1972550,
-      density: '',
-    },
-    {
-      name: 'Japan',
-      code: 'JP',
-      population: 126317000,
-      size: 377973,
-      density: '',
-    },
-    {
-      name: 'France',
-      code: 'FR',
-      population: 67022000,
-      size: 640679,
-      density: '',
-    },
-    {
-      name: 'United Kingdom',
-      code: 'GB',
-      population: 67545757,
-      size: 242495,
-      density: '',
-    },
-    {
-      name: 'Russia',
-      code: 'RU',
-      population: 146793744,
-      size: 17098246,
-      density: '',
-    },
-    {
-      name: 'Nigeria',
-      code: 'NG',
-      population: 200962417,
-      size: 923768,
-      density: '',
-    },
-    {
-      name: 'Brazil',
-      code: 'BR',
-      population: 210147125,
-      size: 8515767,
-      density: '',
-    },
-  ]
+  const rows = chamados.map((chamado) => ({
+    nome: chamado.cliente.nome,
+    assunto: chamado.assunto,
+    status: chamado.status,
+    data: new Date(chamado.dataCriacao).toLocaleDateString('pt-BR'),
+    acoes: '',
+  }))
 
   function handleClick() {
     navigate('/novo-chamado')
@@ -225,14 +139,9 @@ export default function StickyHeadTable() {
             <TableBody>
               {rows
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
+                .map((row, index) => {
                   return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.code}
-                    >
+                    <TableRow hover role="checkbox" tabIndex={-1} key={index}>
                       {columns.map((column) => {
                         return (
                           <TableCell
